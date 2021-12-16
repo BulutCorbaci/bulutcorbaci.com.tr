2ea451071c0bba7cdb79228160199711e430b618
BuluttishTheGuy <78735325+BulutCorbaci@users.noreply.github.com>
2021-12-16 20:20:59 +0300

    Add Signup for Testing

--- src/sign.js
+++ src/sign.js
@@ -1,3 +1,16 @@
+function setFormMessage(formElement, type, message) {
+    const messageElement = formElement.querySelector(".form__message");
+
+    messageElement.textContent = message;
+    messageElement.classList.remove(".form__message--error");
+    messageElement.classList.add('.form__message--${type}').textContent = message;
+}
+
+function setInputError(inputElement, message) {
+    inputElement.classList.add("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error-message");
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.querySelector("#signin");
     const joinForm = document.querySelector("#join");
@@ -11,5 +24,19 @@ document.addEventListener("DOMContentLoaded", () => {
         e.preventDefault();
         loginForm.classList.remove("form__hidden");
         joinForm.classList.add("form__hidden");
+    });
+
+    loginForm.addEventListener("submit", e => {
+        e.preventDefault();
+
+        setFormMessage(loginForm, "error", "Incorrect Username/Password.Try Again.");
+    });
+
+    document.querySelectorAll(".form__input").forEach(inputElement => {
+        inputElement.addEventListener("blur", e => {
+            if (e.target.id === "signupUsername" && e.target.value.lenght > 0 && e.target.value.lenght < 10) {
+
+            }
+        });
     });
 });
